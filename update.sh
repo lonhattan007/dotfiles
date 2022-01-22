e0ebb2cc617819293f40e6b536cdb0f8bbc193cd
Nhat_Tan Lo <lonhattan007@gmail.com>
2022-01-22 22:41:55 +0700

    Add updating script file

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp /usr/share/X11/xorg.conf.d/40-libinput.conf ./40-libinput.conf
+cp ~/.config/alacritty/alacritty.yml ./alacritty.yml
+cp ~/.config/bpytop/bpytop.conf ./bpytop.conf
+cp ~/.config/cava/config ./cava.config
+cp ~/.config/i3/config ./i3-gaps.config
+cp ~/.config/nvim/init.vim ./init.vim
+cp ~/.config/picom.conf ./picom.conf
+cp ~/.config/gtk-3.0/settings.ini ./settings.ini
+cp ~/.tmux.conf ./.tmux.conf
+cp ~/.zshrc ./.zshrc
